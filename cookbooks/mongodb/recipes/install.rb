33aca2327e55712b1e08bc84382d03e0451a4864
millisami <millisami@gmail.com>
2011-09-13 22:20:58 +0545

    mongodb cookbook modified to install it via apt-get package manager instead of downloading and installing from source

--- cookbooks/mongodb/recipes/install.rb
+++ cookbooks/mongodb/recipes/install.rb
@@ -34,27 +34,50 @@ when "centos","redhat","fedora","suse"
   include_recipe "mongodb::10gen_repo"
   package "mongo-10gen-server"
 when "debian","ubuntu"
-  # build from source
+  # installing from package
+  bash "adding mongodb 10gen to /etc/apt/sources.list" do
+    user "root"
+    code <<-EOH
+    echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list
+    apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+    apt-get update
+    EOH
+    only_if do
+      result = File.open("/etc/apt/sources.list").grep(/^deb http:\/\/downloads-distro.mongodb.org/)
+      result.empty?
+    end 
+  end
 
-  if !(::File.exists?("/tmp/#{node[:mongodb][:file_name]}.tgz"))
-    Chef::Log.info "Downloading MongoDB (#{node[:mongodb][:file_name]}.tgz) from #{node[:mongodb][:url]}. This could take a while..."
-    remote_file "/tmp/#{node[:mongodb][:file_name]}.tgz" do
-      source node[:mongodb][:url]
-      not_if { ::File.exists?("/tmp/#{node[:mongodb][:file_name]}.tgz") }
+  bash "installing_mongodb" do
+    user "root"
+    code <<-EOH
+    apt-get install mongodb-10gen
+    EOH
+    not_if do
+      File.exist?(node[:mongodb][:binaries] + "/mongod")
     end
   end
-
-  bash "install-mongodb" do
-    cwd "/tmp"
-    code <<-EOH
-    tar zxvf #{node[:mongodb][:file_name]}.tgz
-    mkdir -p #{node[:mongodb][:binaries]}
-    mv #{node[:mongodb][:file_name]}/bin/* #{node[:mongodb][:binaries]}
-    chmod 755 #{node[:mongodb][:binaries]}
-    EOH
-    not_if { ::File.exists?("#{node[:mongodb][:binaries]}/mongod") && `mongo --version` != "MongoDB shell version: #{node[:mongodb][:version]}"}
-    # not_if { ::File.exists?("#{node[:mongodb][:binaries]}/mongod") }
-  end
+  
+  
+  # if !(::File.exists?("/tmp/#{node[:mongodb][:file_name]}.tgz"))
+  #   Chef::Log.info "Downloading MongoDB (#{node[:mongodb][:file_name]}.tgz) from #{node[:mongodb][:url]}. This could take a while..."
+  #   remote_file "/tmp/#{node[:mongodb][:file_name]}.tgz" do
+  #     source node[:mongodb][:url]
+  #     not_if { ::File.exists?("/tmp/#{node[:mongodb][:file_name]}.tgz") }
+  #   end
+  # end
+  # 
+  # bash "install-mongodb" do
+  #   cwd "/tmp"
+  #   code <<-EOH
+  #   tar zxvf #{node[:mongodb][:file_name]}.tgz
+  #   mkdir -p #{node[:mongodb][:binaries]}
+  #   mv #{node[:mongodb][:file_name]}/bin/* #{node[:mongodb][:binaries]}
+  #   chmod 755 #{node[:mongodb][:binaries]}
+  #   EOH
+  #   not_if { ::File.exists?("#{node[:mongodb][:binaries]}/mongod") && `mongo --version` != "MongoDB shell version: #{node[:mongodb][:version]}"}
+  #   # not_if { ::File.exists?("#{node[:mongodb][:binaries]}/mongod") }
+  # end
 end
 
 # Add bin directory to everyone's path for bash
